2db9f7bdbfbcef03609b09e9b1a2eb65758eb5ce
dmalis18 <dmalis@umich.edu>
2023-12-01 17:13:16 -0500

    Fixed

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from pybaseball import bwar_bat
 from pybaseball import bwar_pitch
 from pybaseball import amateur_draft_by_team
 import sqlite3
+import csv
+import numpy as np
 import json
 import os
 
@@ -29,24 +31,39 @@ def read_round_draft_data_specific_year(year, round, cur, conn):
         pos = draftResults["Pos"][i]
         tm = draftResults["Tm"][i]
         primary_key = int(str(year) + str(round) + str(ovPck))
-        cur.execute("INSERT OR IGNORE INTO DRAFTED_BY_ROUND (id, name, position, team, year, round) VALUES (?, ?, ?, ?, ?, ?)", (primary_key, name, pos, tm, year, round))
-    conn.commit()
+    #     cur.execute("INSERT OR IGNORE INTO DRAFTED_BY_ROUND (id, name, position, team, year, round) VALUES (?, ?, ?, ?, ?, ?)", (primary_key, name, pos, tm, year, round))
+    # conn.commit()
     for item in cur.execute("SELECT * FROM DRAFTED_BY_ROUND"):
         print(item)
 
-def read_team_draft_data(team_name, year, cur, conn):
+def read_team_draft_data(team_name, year):
     draft_results = amateur_draft_by_team(team_name, year)
-    for i in range(len(draft_results)):
-        # OvPck, Tm, Year, Round
-        OvPck = draft_results["OvPck"][i]
-        name = draft_results["Name"][i]
-        Tm = draft_results["Tm"][i]
-        Round = draft_results["Round"][i]
-        pos = draft_results["Pos"][i]
-        tm = draft_results["Tm"][i]
-        primary_key = int(str(year) + str(Round) + str(OvPck))
-        cur.execute("INSERT OR IGNORE INTO DRAFTED_BY_ROUND (id, name, position, team, year, round) VALUES (?, ?, ?, ?, ?, ?)", (primary_key, name, pos, tm, year, round))
-    conn.commit()
+
+    with open('draft_data.txt', 'a', newline='') as csvfile:
+        fieldnames = ['id', 'Year', 'OvPck', 'Name', 'Tm', 'Round', 'Pos', 'G']  # Add the field names as per your data
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        for i in range(len(draft_results)):
+            # OvPck, Tm, Year, Round
+            OvPck = draft_results["OvPck"][i]
+            name = draft_results["Name"][i]
+            tm = draft_results["Tm"][i]
+            pos = draft_results["Pos"][i]
+            games = draft_results["G"][i]
+            signed = draft_results["Signed"][i]
+            if signed == "N":
+                continue
+            if np.isnan(games):
+                games = False
+            else:
+                games = True
+
+            primary_key = int(str(year) + str(OvPck))
+
+            # Write the data to the CSV file
+            writer.writerow({'OvPck': OvPck, 'Year': year, 'Name': name, 'Tm': tm, 'Pos': pos, 'G': games, 'id': primary_key})
+
+    print("Data has been written to draft_data.txt")
 
 def set_up_database(db_name):
     """
@@ -98,6 +115,21 @@ def set_up_team_draft_data(cur, conn):
     )
     conn.commit()
 
+def get_all_needed_draft_data():
+    teams = ['ANA','HOU','OAK','TOR','ATL','MIL', 'STL','CHC','TBD','ARI','LAD','SFG','CLE','SEA','FLA','NYM','WSN','BAL','SDP','PHI','PIT','TEX','BOS','CIN','COL','KCR','DET','MIN','CHW','NYY']
+    with open('draft_data.txt', 'w', newline='') as csvfile:
+        fieldnames = ['id', 'Year', 'OvPck', 'Name', 'Tm', 'Round', 'Pos', 'G']  # Add the field names as per your data
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+    for team in teams:
+        for year in range(1990, 2016):
+            try:
+                read_team_draft_data(team, year)
+            except:
+                print(team, year)
+                
+
 def main():
     """
     Main execution function.
@@ -114,9 +146,7 @@ def main():
     # Set up database
     cur, conn = set_up_database("baseball.db")
     set_up_player_draft_data(cur, conn)
-    #read_round_draft_data_specific_year(2017, 3, cur, conn)
-    read_team_draft_data("TBD", 2011, cur, conn)
-    #get_bwar_data()
+    # get_all_needed_draft_data()
 
     # Close database connection
     conn.close()
