1624349a7ce062ebc6d73cf4f475eeb4d51a4a6f
dmalis18 <dmalis@umich.edu>
2023-12-03 16:09:40 -0500

    Cleaned up files

--- main.py
+++ main.py
@@ -1,40 +1,21 @@
 import unittest
 from pybaseball import amateur_draft
-from pybaseball import bwar_bat
-from pybaseball import bwar_pitch
 from pybaseball import amateur_draft_by_team
 import sqlite3
 import csv
 import numpy as np
 import json
 import os
+import requests
 
-def read_round_draft_data_specific_year(year, round, cur, conn):
-    """
-    Reads data from a file with the given filename.
+def populate_teams(cur, conn):
+    pass
 
-    Parameters
-    -----------------------
-    filename: str
-        The name of the file to read.
+def populate_if_team_drafted_data(cur, conn):
+    pass
 
-    Returns
-    -----------------------
-    dict:
-        Parsed JSON data from the file.
-    """
-    draftResults = amateur_draft(year, round)
-        # Grab OvPck, Name, Pos, Tm
-    for i in range(len(draftResults)):
-        ovPck = draftResults["OvPck"][i]
-        name = draftResults["Name"][i]
-        pos = draftResults["Pos"][i]
-        tm = draftResults["Tm"][i]
-        primary_key = int(str(year) + str(round) + str(ovPck))
-    #     cur.execute("INSERT OR IGNORE INTO DRAFTED_BY_ROUND (id, name, position, team, year, round) VALUES (?, ?, ?, ?, ?, ?)", (primary_key, name, pos, tm, year, round))
-    # conn.commit()
-    for item in cur.execute("SELECT * FROM DRAFTED_BY_ROUND"):
-        print(item)
+def populate_team_draft_data(cur, conn):
+    pass
 
 def read_team_draft_data(team_name, year):
     draft_results = amateur_draft_by_team(team_name, year)
@@ -84,34 +65,21 @@ def set_up_database(db_name):
     cur = conn.cursor()
     return cur, conn
 
-
-def set_up_player_draft_data(cur, conn):
-    """
-    Sets up the Types table in the database using the provided Pokemon data.
-
-    Parameters
-    -----------------------
-    data: list
-        List of Pokemon data in JSON format.
-
-    cur: Cursor
-        The database cursor object.
-
-    conn: Connection
-        The database connection object.
-
-    Returns
-    -----------------------
-    None
-    """
-    cur.execute(
-        "CREATE TABLE IF NOT EXISTS DRAFTED_BY_ROUND (id INTEGER PRIMARY KEY, name TEXT, position TEXT, team TEXT, year INTEGER, round INTEGER)"
-    )
-    conn.commit()
-
 def set_up_team_draft_data(cur, conn):
     cur.execute(
         "CREATE TABLE IF NOT EXISTS DRAFTED_BY_TEAM (id INTEGER PRIMARY KEY, draft_pick INTEGER, name TEXT)"
+    )
+    conn.commit()
+
+def set_up_if_team_drafted_data(cur, conn):
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS TEAM_DRAFTED (id INTEGER PRIMARY KEY, draft_pick INTEGER, name TEXT)"
+    )
+    conn.commit()
+
+def set_up_teams(cur, conn):
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS TEAMS(id INTEGER PRIMARY KEY, team_name TEXT)"
     )
     conn.commit()
 
@@ -128,6 +96,33 @@ def get_all_needed_draft_data():
                 read_team_draft_data(team, year)
             except:
                 print(team, year)
+
+def read_active_teams(start_year:int, end_year:int, cur, conn):
+    # build set of all teams from 2022
+    all_teams_set = set()
+    resp = requests.get(f"http://lookup-service-prod.mlb.com/json/named.team_all_season.bam?sport_code='mlb'&all_star_sw='N'&sort_order=name_asc&season='2022'")
+    data = resp.json()
+    i = 0
+    for team in data['team_all_season']['queryResults']['row']:
+        all_teams_set.add(team['mlb_org'])
+        i += 1
+        
+    # build set of teams that existed in the start year
+    teams = set()
+    year = f"'{str(start_year)}'"
+    resp = requests.get(f"http://lookup-service-prod.mlb.com/json/named.team_all_season.bam?sport_code='mlb'&all_star_sw='N'&sort_order=name_asc&season={year}")
+    data = resp.json()
+    for team in data['team_all_season']['queryResults']['row']:
+        teams.add(team['mlb_org'])
+    
+    # print to csv if team has existed since start year til 2022
+    with open("valid_teams.txt", "w") as file:
+        file.write("Team,Valid\n")
+        for team in all_teams_set:
+            if team in all_teams_set.intersection(teams):
+                file.write(f"{team},True\n")
+            else:
+                file.write(f"{team},False\n")
                 
 
 def main():
@@ -145,8 +140,14 @@ def main():
 
     # Set up database
     cur, conn = set_up_database("baseball.db")
-    set_up_player_draft_data(cur, conn)
+    set_up_team_draft_data(cur, conn)
+    set_up_if_team_drafted_data(cur, conn)
+    set_up_teams(cur, conn)
     # get_all_needed_draft_data()
+    # read_active_teams(1990, 2015, cur, conn)
+    populate_teams(cur, conn)
+    populate_if_team_drafted_data(cur, conn)
+    populate_team_draft_data(cur, conn)
 
     # Close database connection
     conn.close()
